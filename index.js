bd2c231a1eef917fed155b10f1f69f97f6b2bb70
Konstantin-exe <konstantinmuellner@gmail.com>
2021-01-20 19:41:58 +0100

    crawler works, images as strings are in array

--- index.js
+++ index.js
@@ -1,6 +1,49 @@
 const fetch = require('node-fetch');
 const url = 'https://memegen-link-examples-upleveled.netlify.app/';
+const jsdom = require('jsdom');
+const { JSDOM } = jsdom;
+const imgArr = [];
 
-fetch(url)
-  .then((res) => res.text())
-  .then((body) => console.log(body));
+async function getAny() {
+  const response = await fetch(url);
+  const body = await response.text();
+  imgArr.push(body);
+  const dom = new JSDOM(body);
+  console.log(
+    [...dom.window.document.querySelectorAll('img')].map(
+      (img) => img.outerHTML.split('https')[1],
+    ),
+  );
+}
+
+getAny();
+
+// async function getUserAsync(name) {
+//   const response = await fetch(url);
+//   const body = await response.json();
+
+//   return body;
+// }
+
+// getUserAsync();
+
+// async function getBody() {
+//   const Data = await await fetch(url)
+//     .then((res) => res.text())
+//     .then(function (body) {
+//       return body;
+//     });
+//   return await Data;
+// }
+
+// console.log(getBody());
+
+// // const dom = new JSDOM(string);
+
+// const dom = new JSDOM(``, {
+//   url: url,
+//   referrer: url,
+//   contentType: 'text/html',
+//   includeNodeLocations: true,
+//   storageQuota: 10000000,
+// });
