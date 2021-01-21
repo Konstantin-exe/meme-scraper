10256eb650e5a5d874824eebefa21ca8c3ca2172
Konstantin-exe <konstantinmuellner@gmail.com>
2021-01-21 18:30:40 +0100

    projects running, working on read me and stretch goals

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const fetch = require('node-fetch');
 const fs = require('fs');
 const request = require('request');
+const path = require('path');
 
 const url = 'https://memegen-link-examples-upleveled.netlify.app/';
 
 let imgArr = [];
 
+// making directory for memes
+let dir = './memes';
+if (!fs.existsSync(dir)) {
+  fs.mkdirSync(dir);
+}
+
+// helper function to build an array with strings of <img src=/>
 function getImg(arr) {
   let elementArr = [];
   for (let element of arr) {
@@ -17,31 +25,31 @@ function getImg(arr) {
   return elementArr;
 }
 
+// request for img download
+const download = function (uri, filename, callback) {
+  request.head(uri, function (err, res, body) {
+    console.log('content-type:', res.headers['content-type']);
+    console.log('content-length:', res.headers['content-length']);
+
+    request(uri).pipe(
+      fs
+        .createWriteStream(path.join(__dirname, './memes', filename))
+        .on('close', callback),
+    );
+  });
+};
+// fetching the HTML
 async function scrapMemes() {
   const res = await fetch(url);
   const html = await res.text();
   const htmlArr = html.split('\n');
   imgArr = getImg(htmlArr);
   console.log(imgArr);
+
+  for (let i = 0; i < imgArr.length; i++) {
+    download(imgArr[i], `meme ${i}.jpg`, function () {
+      console.log('done');
+    });
+  }
 }
-
 scrapMemes();
-
-// fs.mkdir('./memes');
-
-const download = function (uri, filename, callback) {
-  request.head(uri, function (err, res, body) {
-    console.log('content-type:', res.headers['content-type']);
-    console.log('content-length:', res.headers['content-length']);
-
-    request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
-  });
-};
-
-download(
-  'https://www.google.com/images/srpr/logo3w.png',
-  'google.png',
-  function () {
-    console.log('done');
-  },
-);
