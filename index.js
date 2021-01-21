07e8390ee9623533c15aa86682628da39d339748
Konstantin-exe <konstantinmuellner@gmail.com>
2021-01-21 15:52:47 +0100

    loop done / 10 img in array

--- index.js
+++ index.js
@@ -1,27 +1,24 @@
 const fetch = require('node-fetch');
 const fs = require('fs');
 const url = 'https://memegen-link-examples-upleveled.netlify.app/';
+let imgArr = [];
 
 async function scrapMemes() {
   const res = await fetch(url);
   const html = await res.text();
-  //const searchMatch = '<img[^>]+src\\s*=\\s*[\'"]([^\'"]+)[\'"][^>]*>';
-  let imgArr = html.split('\n');
-  imgArr = [getImg(imgArr)];
+  let htmlArr = html.split('\n');
+  imgArr = getImg(htmlArr);
   console.log(imgArr);
+}
+function getImg(arr) {
+  let elementArr = [];
+  for (let element of arr) {
+    if (element.match('<img[^>]+src\\s*=\\s*[\'"]([^\'"]+)[\'"][^>]*>')) {
+      elementArr.push(element);
+    }
+  }
+  elementArr.length = 10;
+  return elementArr;
 }
 
 scrapMemes();
-// imgArr.forEach(function (element) {
-//   if (element.match("img") {
-//     arr.push(element)
-//   })
-// })
-
-function getImg(arr) {
-  for (let element of arr) {
-    if (element.match('<img[^>]+src\\s*=\\s*[\'"]([^\'"]+)[\'"][^>]*>')) {
-      return element;
-    }
-  }
-}
