f8e246a1ceb710e3ad65bf4208365cabdbe80e18
Konstantin-exe <konstantinmuellner@gmail.com>
2021-01-21 09:13:04 +0100

    img in arr, needs additional loop?

--- index.js
+++ index.js
@@ -2,12 +2,26 @@ const fetch = require('node-fetch');
 const fs = require('fs');
 const url = 'https://memegen-link-examples-upleveled.netlify.app/';
 
-// let imgArr = [];
 async function scrapMemes() {
   const res = await fetch(url);
   const html = await res.text();
-  const imgArr = html.split('\n').filter('<img');
+  //const searchMatch = '<img[^>]+src\\s*=\\s*[\'"]([^\'"]+)[\'"][^>]*>';
+  let imgArr = html.split('\n');
+  imgArr = [getImg(imgArr)];
   console.log(imgArr);
 }
 
 scrapMemes();
+// imgArr.forEach(function (element) {
+//   if (element.match("img") {
+//     arr.push(element)
+//   })
+// })
+
+function getImg(arr) {
+  for (let element of arr) {
+    if (element.match('<img[^>]+src\\s*=\\s*[\'"]([^\'"]+)[\'"][^>]*>')) {
+      return element;
+    }
+  }
+}
